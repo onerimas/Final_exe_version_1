5cf58f142b12800edb0a16c8390a89a951aba573
onerimas <44803100+onerimas@users.noreply.github.com>
2019-08-24 15:57:00 +0200

    Add files via upload

--- main/live_trained_NN.py
+++ main/live_trained_NN.py
@@ -142,126 +142,95 @@ def youtube_f1 (numarray):
     #     return numarray[5434]
     # else:
     #     return 0
-    return numarray[5434] if numarray[5434]>0 else 0
+    return math.log(numarray[1038]+numarray[1100], 10) if numarray[1038]+numarray[1100]>0 else 0
 
 def youtube_f2 (numarray):
-    x=numarray[3521]+numarray[3522]+numarray[3523]+numarray[3516]
+    x=numarray[1212]+numarray[1211]+numarray[1210]
     if x>2.1:
         return x
     else:
         return 0
 
 def youtube_f3 (numarray):
-    x=numarray[2849]+numarray[2850]+numarray[2851]+numarray[4571]
+    x=numarray[1458]+numarray[1459]+numarray[1460]+numarray[1461]
     if x>0:
         return x
     else:
         return 0
 
 def youtube_f4 (numarray):
-    x=numarray[2513]+numarray[2514]+numarray[2515]+numarray[2516]+numarray[2517]
+    x=numarray[1822]+numarray[1823]+numarray[1824]+numarray[1825]
     if x>1:
-        return math.log(x,5)
-    else:
-        return 0
-
-def youtube_f5 (numarray):
-    x=op_max_checker(4100,numarray)
-    if x>5:
-        return numarray[4100]
-    else:
-        return 0
-
-#added 3660 and 3664 because of testbed
-def youtube_f6 (numarray):
-    x = numarray[3661] + numarray[3662] + numarray[3663]
-    if x>3:
-        return math.log(x,5)
-    else:
-        return 0
-
-def youtube_f7 (numarray):
-    x=0
-    x=range_area(2485,2493,numarray)
-    #x=numarray[2485]+numarray[2493]
-    if x>3:
-        return math.log(x,10)
-    else:
-        return 0
-#following cunctions were added to handle testbed data ===========
-def youtube_f8(numarray):
-    x = numarray[4113] + numarray[4112]
-    return x if x>0 else 0
-
-def youtube_f9(nummarray):
-    x = nummarray[3418] + nummarray[3204] + nummarray[3106]
-    return x if x>0 else 0
-
-def youtube_f10(numarray):
-    x = range_area(5100,5650,numarray)
-    return x if x>0 else 0
-
-def youtube_f11 (numarray):
-    x = numarray[3863] + numarray[3859]
-    return x if x>0 else 0
-#End of youtube features ////////////////////////////////////
-
-#Facebook features/////////////////////////////////////////
-def facebook_f1 (numarray):
-    x=op_max_checker(3903,numarray)
-    if x>5:
-        return math.log(x,10)
-    else:
-        return 0
-
-def facebook_f2 (numarray):
-    if (numarray[2847])>0:
-        return math.log(numarray[2847],5)
-    else:
-        return 0
-
-def facebook_f3 (numarray):
-    if (numarray[2626])>0:
-        x=math.log(numarray[2626],5)
         return x
     else:
         return 0
 
+def youtube_f5 (numarray):
+    x=numarray[2487]+numarray[2517]
+    if x>0:
+        return math.log(x, 10)
+    else:
+        return 0
+
+def youtube_f6 (numarray):
+    x=numarray[2658]+numarray[2659]+numarray[2660]+numarray[2661]
+    if x>1:
+        return x
+    else:
+        return 0
+
+#End of youtube features ////////////////////////////////////
+
+#Facebook features/////////////////////////////////////////
+def facebook_f1 (numarray):
+    if (numarray[1138])>0:
+        return math.log(numarray[1138],10)
+    else:
+        return 0
+
+def facebook_f2 (numarray):
+    if (numarray[2536])>0:
+        return math.log(numarray[2536],10)
+    else:
+        return 0
+
+def facebook_f3 (numarray):
+    if (numarray[5318]+numarray[5319])>0:
+        return numarray[5318]+numarray[5319]
+    else:
+        return 0
+
 def facebook_f4 (numarray):
-    if numarray[2535]>0:
-        x=math.log(numarray[2535],10)
+    if numarray[5465]+numarray[5466]>0:
+        x=math.log(numarray[5465]+numarray[5466],2)
         return x
     else:
         return 0
 
 def facebook_f5 (numarray):
-    x=0
-    x=range_area(4193,4203,numarray)
-    if x>0:
+    if (numarray[3739]+numarray[3740]+numarray[3741]+numarray[3742]+numarray[3743])>0:
         #math.log(x,10)
-        return x/10
+        return (numarray[3739]+numarray[3740]+numarray[3741]+numarray[3742]+numarray[3743])/10
     else:
         return 0
 
 def facebook_f6 (numarray):
-    if (numarray[5464]+numarray[5494]+numarray[5506])>0:
-        x=math.log(numarray[5464]+numarray[5494]+numarray[5506],5)
-        return x
+    if (numarray[3753]+numarray[3754])>0:
+        return numarray[3753]+numarray[3754]
     else:
         return 0
 
-#following cunctions were added to handle testbed data ===========
-def facebook_f7(numarray):
-    x = numarray[3899] + numarray[3900]
-    return x if x>0 else 0
+def facebook_f6 (numarray):
+    if (numarray[3753]+numarray[3754])>0:
+        return numarray[3753]+numarray[3754]
+    else:
+        return 0
 
-def facebook_f8(numarray):
-    x = range_area(4302,4334,numarray)
-    return x if x>2 else 0
-
-def facebook_f9(numarray):
-    x = numarray[3904] + numarray[3905] + numarray[3906]
-    return x if x>0 else 0
+def facebook_f7 (numarray):
+    if (numarray[3899]+numarray[3900])>0:
+        return numarray[3899]+numarray[3900]
+    else:
+        return 0
 #End of facebook features//////////////////////////////////
 
 #Whatsapp features //////////////////////////////////////////
@@ -277,8 +246,8 @@ def whatsapp_f1f2f3 (numarray):
 
     #index, value = mf.range_max_index(3770,3832,numarray)
     if x>4 and (numarray[4194]+numarray[4196]+numarray[4205])<10 and numarray[3861]+numarray[3859] == 0:
-        wstd=range_std(3780,3859,numarray)
-        warea=range_area(3780,3859,numarray)
+        wstd= range_std(3780,3859,numarray)
+        warea= range_area(3780,3859,numarray)
         return x,wstd,warea/10
     else:
         return 0,0,0
@@ -329,7 +298,7 @@ def whatsapp_f9 (numarray):
         return 0
 
 def whatsapp_f10f11f12 (numarray):
-    x=range_nzlength(4109,4177,numarray,5)
+    x= range_nzlength(4109,4177,numarray,5)
     index, value = range_max_index(4079,4159,numarray)
 
     if x>4 and (index+4079)>4119 and numarray[2535]+numarray[4114]==0:
@@ -402,6 +371,7 @@ def skypeVDOC_f1f2f3 (numarray):
     svdoc_area=range_area(2800,3400,numarray)
     return x,svdoc_sd,math.log(svdoc_area,10) if svdoc_area>0 else 0
 
+
 def skypeVDOC_f4f5f6 (numarray):
     x=0
     for i in range(4500,5100):
@@ -414,11 +384,13 @@ def skypeVDOC_f4f5f6 (numarray):
     svdoc_area=range_area(4500,5100,numarray)
     #svdoc_area_log=math.log(svdoc_area,10)
     return x,svdoc_sd,math.log(svdoc_area,10) if svdoc_area>0 else 0
+
+
 #End of Skype vedio features/////////////////////////////////
 
 
 while True:
-    starttime=time.time()
+    #starttime=time.time() #timer start to exicute the code periodicaly
     tf.reset_default_graph()
     #///////////////////////////////////////////////////////////////////////////
     #pkts_list=sa.sniff(timeout=5)
@@ -496,13 +468,9 @@ while True:
     features.append(youtube_f4(listbukket1))
     features.append(youtube_f5(listbukket1))
     features.append(youtube_f6(listbukket1))
-    features.append(youtube_f7(listbukket1))
-    features.append(youtube_f8(listbukket1))
-    features.append(youtube_f9(listbukket1))
-    features.append(youtube_f10(listbukket1))
-    features.append(youtube_f11(listbukket1))
-    # features.append(features[0] + features[1] + features[2] + features[3] + features[4] + features[5]
-    #                 + features[6] + features[7] + features[8] + features[9] + features[10])
+
+    features.append(features[0] + features[1] + features[2] + features[3] + features[4] + features[5]
+                    + features[6])
     #Facebook Vedio ===========================================================
     features.append(facebook_f1(listbukket1))
     features.append(facebook_f2(listbukket1))
@@ -511,10 +479,9 @@ while True:
     features.append(facebook_f5(listbukket1))
     features.append(facebook_f6(listbukket1))
     features.append(facebook_f7(listbukket1))
-    features.append(facebook_f8(listbukket1))
-    features.append(facebook_f9(listbukket1))
-    # features.append(features[12] + features[13] + features[14] + features[15] + features[16] + features[17]
-    #                 + features[18] + features[19] + features[20])
+
+    features.append(features[8] + features[9] + features[10] + features[11] + features[12] + features[13]
+                    + features[14])
     #WhatsApp call===========================================================
     x,y,z = whatsapp_f1f2f3(listbukket1)
     features.append(x)
@@ -530,8 +497,8 @@ while True:
     features.append(x)
     features.append(y)
     features.append(z)
-    # features.append(features[22] + features[23] + features[24] + features[25] + features[26] + features[27]
-    #                 + features[28] + features[29] + features[30] + features[31] + features[32] + features[33])
+    features.append(features[16] + features[17] + features[18] + features[19] + features[20] + features[21]
+                    + features[22] + features[23] + features[24] + features[25] + features[26] + features[27])
     #Skype call===========================================================
     x,y,z = skype_f1f2f3(listbukket1)
     features.append(x)
@@ -545,8 +512,8 @@ while True:
     features.append(skype_f8(listbukket1))
     features.append(skype_f9(listbukket1))
     features.append(skype_f10(listbukket1))
-    # features.append(features[35] + features[36] + features[37] + features[38] + features[39] + features[40]
-    #                 + features[41] + features[42] + features[43] + features[44])
+    features.append(features[29] + features[30] + features[31] + features[32] + features[33] + features[34]
+                    + features[35] + features[36] + features[37] + features[38])
     #Skype Vedio call===========================================================
     x,y,z=skypeVDOC_f1f2f3(listbukket1)
     features.append(x)
@@ -556,7 +523,7 @@ while True:
     features.append(x)
     features.append(y)
     features.append(z)
-    #features.append(features[46] + features[47] + features[48] + features[49] + features[50] + features[51])
+    features.append(features[40] + features[41] + features[42] + features[43] + features[44] + features[45])
     #//////////shrinking the concatienated llist
     #new=[]
     #new=mf.shrink(listbukket1,4)
@@ -578,7 +545,7 @@ while True:
     # labels_test = (np.arange(1) == nn_testtargets[:,None]).astype(np.float32)
 
     # input layer
-    hid1_size = 48
+    hid1_size = 60
     w1 = tf.Variable(tf.random_normal([hid1_size, my_list.shape[1]],stddev= 0.1, seed=1), name='w1')
     b1 = tf.Variable(tf.random_normal(shape=(hid1_size, 1)), name='b1')
     y1 = tf.nn.sigmoid(tf.add(tf.matmul(w1, tf.transpose(inputs)), b1))
@@ -649,5 +616,5 @@ while True:
         # with open("C:\\Users\\Rimas\\Desktop\\predictions\\MLPpredictionsLoaded.csv", "a") as output:
         #     writer=csv.writer(output,lineterminator='\n')
         #     writer.writerows(new)
-    time.sleep(10 - ((time.time()-starttime)%60))
+    #time.sleep(10 - ((time.time()-starttime)%60)) # get the time taken by the algorithm and sleep remaining seconds
 
